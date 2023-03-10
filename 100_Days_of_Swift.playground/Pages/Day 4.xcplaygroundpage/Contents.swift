1eb4d816af46f835cb83a7c41c3fb38840e73cc8
smichaelonline <stephaniemichael001@gmail.com>
2023-03-09 20:25:02 -0500

    Day 4: Loops - run through for and while loops

--- 100_Days_of_Swift.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
+++ 100_Days_of_Swift.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
@@ -1,2 +1,40 @@
 import UIKit
 
+// Loops run code repeatedly until a condition evaluates as false
+
+// FOR LOOPS
+  let count = 1...10
+
+  // prints each of the numbers in count
+  /*
+     for number in count {
+      print("Number is \(number)")
+     }
+     
+     let albums = ["Red", "1989", "Reputation"]
+     
+     for album in albums {
+      print("\(album) is on Apple Music")
+     }
+   
+   // you can use an _ instead of a constant
+     print("Players gonna ")
+     
+     for _ in 1...5 {
+      print("play")
+     }
+   */
+
+// WHILE LOOPS
+  // while: give it a condition to check, and its loop code will go around and around until the condition fails.
+
+  /*
+    var number = 1
+    while number <= 20 {
+      print(number)
+      number += 1
+    }
+
+    print ("Ready or not, here I come!")
+   */
+
